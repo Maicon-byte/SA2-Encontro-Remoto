b9209033f4c249cbe27659738c573b1b8f356465
Maicon Silva de Sousa <maiconsilva2014.ms@gmail.com>
2022-04-08 18:06:38 -0300

    Primeiro commit

---
+++ cadastro.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        Seu nome: <?php echo $_POST["nome"]; ?><br/>
+        Seu email: <?php echo $_POST["email"]; ?><br/>
+        <br/>
+        Concorda com os termos: <?php echo $_POST["yes"]; ?>
+    </body>
+</html>
