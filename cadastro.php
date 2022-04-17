aba8d5ed36e8ed29b1bf4acba6909f7e25397f33
Maicon Silva de Sousa <maiconsilva2014.ms@gmail.com>
2022-04-17 13:05:19 -0300

    Algumas alteraçoes antes de entregar

--- cadastro.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-        Seu nome: <?php echo $_POST["nome"]; ?><br/>
-        Seu email: <?php echo $_POST["email"]; ?><br/>
-        <br/>
-        Concorda com os termos: <?php echo $_POST["yes"]; ?>
-    </body>
-</html>
