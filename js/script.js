a86cfc575146709909b0837b4d40a2ff7846cb71
Maicon Silva de Sousa <maiconsilva2014.ms@gmail.com>
2022-06-09 18:12:24 -0300

    Adicionando o Jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+$(document).ready(function(){
+
+  let slideAtual = 1;
+  let listaSlides = ["banner-02", "banner-03","banner-09"]
+
+  setInterval(mudarSlide, 1000)
+
+  function mudarSlide(){
+
+    if (slideAtual > 0){
+      $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+    } else if(slideAtual == 0){
+      $("#carrossel").removeClass(listaSlides[(listaSlides.length) -1])
+    }
+
+    $("#carrossel").addClass(listaSlides[slideAtual]);
+
+    slideAtual++
+
+    if(slideAtual > (listaSlides.length) -1){
+      slideAtual = 0;
+    }
+
+  }
+
+})
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
